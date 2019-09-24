fc5dbe9380363f6f52acaa4a64976c18bd36e5cf
James Jacob <jayj1121@gmail.com>
2019-09-23 21:16:07 -0400

    Created test harness project

---
+++ TestHarness/TestHarness.cpp
@@ -0,0 +1,13 @@
+// TestHarness.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+#include "Common.h"
+#include "Game.h"
+#include "Parser.h"
+
+int main()
+{
+
+}
